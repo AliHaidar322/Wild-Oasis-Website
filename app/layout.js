4b09c2ade7803b4c84f2025ed8ecd7d8eaab9a09
alihaidar322 <bcsf19a020@pucit.edu.pk>
2024-10-22 16:17:29 +0500

    ** Added Logo component ** Added Navigation and Logo to layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,16 @@
+import Logo from './components/Logo';
+import Navigation from './components/Navigation';
+
+export const metadata = {
+  title: 'The Wild Oasis',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Logo />
+        <Navigation />
         {children}
       </body>
     </html>
