1e4590f6e53f31b23d39f037b8d94bfcfb7573ad
alihaidar322 <bcsf19a020@pucit.edu.pk>
2024-10-22 16:19:25 +0500

    Moved links in Navigation to list items

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -3,10 +3,20 @@ import Link from "next/link";
 export default function Page() {
   return(
     <>
-      <Link href="/">Home</Link>
-      <Link href="/cabins">Cabins</Link>
-      <Link href="/account">Account</Link>
-      <Link href="/about">About</Link>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/cabins">Cabins</Link>
+        </li>
+        <li>
+          <Link href="/account">Account</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
     </>
   );
 }
